4a8ed3b05d5b8c3bb6fae0976610e25ecd15525f
AbdouVak <abdou.vakouiev@gmail.com>
2023-04-20 14:21:37 +0200

    teste des fonction de la classe compte banquaire

--- index.php
+++ index.php
@@ -7,5 +7,12 @@ et leurs comptes bancaires respectifs</p>
 
 
 <?PHP
+include ('titulaire.php');
+include ('compteBanquaire.php');
 
+$King = new Titulaire("Stephen","King","1950-05-16","New-York");
+$compteCourantKing = new compteBanque("compteCourant",2000,"dollar",$King);
+$livretAKing = new compteBanque("LivretA",250,"dollar",$King);
+
+echo $King
 ?>
