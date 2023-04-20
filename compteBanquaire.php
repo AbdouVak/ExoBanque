7e3bd3a3d26ebbd03368895323a095e8cb8ffd66
AbdouVak <abdou.vakouiev@gmail.com>
2023-04-20 15:30:33 +0200

    meilleur visiblité du code  + amelioration de de la fonction virement

--- compteBanquaire.php
+++ compteBanquaire.php
@@ -16,41 +16,53 @@ class compteBanque{
     }
 
     /*Création des methode getter et setter*/ 
-    public function getLibelle(): string{return $this->_libelle;}
-    public function setLibelle(string $libelle){$this->_libelle = $libelle;}
+    public function getLibelle(): string{
+        return $this->_libelle;
+    }
+    public function setLibelle(string $libelle){
+        $this->_libelle = $libelle;
+    }
 
-    public function getSoldeInitial(): string{return $this->_soldeInitial;}
-    public function setSoldeInitial(string $soldeInitial){$this->_soldeInitial = $soldeInitial;}
+    public function getSoldeInitial(): string{
+        return $this->_soldeInitial;
+    }
+    public function setSoldeInitial(string $soldeInitial){
+        $this->_soldeInitial = $soldeInitial;
+    }
 
-    public function getDeviseMonetaire(): string{return $this->_deviseMonetaire;}
-    public function setDeviseMonetaire(string $deviseMonetaire){$this->_deviseMonetaire = $deviseMonetaire;}
+    public function getDeviseMonetaire(): string{
+        return $this->_deviseMonetaire;
+    }
+    public function setDeviseMonetaire(string $deviseMonetaire){
+        $this->_deviseMonetaire = $deviseMonetaire;
+    }
 
-    public function getTitulairee(): string{return $this->_titulaire;}
-    public function setTitulaire(string $titulaire){$this->_titulaire = $titulaire;}
+    public function getTitulairee(): string{
+        return $this->_titulaire;
+    }
+    public function setTitulaire(string $titulaire){
+        $this->_titulaire = $titulaire;
+    }
 
 
     /*Création de la methode pour créditer un compte*/ 
-    public function crediter($sommeAjouter){
+    public function crediter(float $sommeAjouter){
         $this->_soldeInitial = $this->_soldeInitial + $sommeAjouter;
         return "Vous avez crédité votre compte  $this->_libelle de  $sommeAjouter $this->_deviseMonetaire<br>";
     }
 
     /*Création de la methode pour débiter un compte*/ 
-    public function debiter($sommeRetirer){
+    public function debiter(float $sommeRetirer){
         $this->_soldeInitial = $this->_soldeInitial + $sommeRetirer;
         return "Vous avez debité votre compte  $this->_libelle de  $sommeRetirer $this->_deviseMonetaire<br>";
 
     }
 
     /*Création de la methode pour faire un virement d'un compte titulaire a un autres*/ 
-    public function virement($compteDestinataire,$sommeVirer){
-        foreach($this->_titulaire->getCompteBanquaire() as $compte){
-            if($compte == $compteDestinataire){
-                $this->debiter($sommeVirer);
-                $compte->crediter($sommeVirer);
-                return "Vous avez fait un virement de $sommeVirer $this->_deviseMonetaire de votre compte  $this->_libelle a votre compte $compte->_libelle ";
-            }
-        }
+    public function virement(compteBanque $compteDestinataire, float  $sommeVirer){
+        $this->debiter($sommeVirer);
+        $compteDestinataire->crediter($sommeVirer);
+        return "Vous avez fait un virement de $sommeVirer $this->_deviseMonetaire de votre compte  $this->_libelle a votre compte $compteDestinataire->_libelle ";
     }
 
     /*Utilisation de fonction toString pour afficher les information du compte banquaire*/ 
